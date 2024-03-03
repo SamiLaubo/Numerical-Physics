e1a73c6524a55620962d0268587f957513447707
Sami Laubo <slaubo99@gmail.com>
2024-03-03 12:31:15 +0100

    Find roots. Task 3.4-3.6

---
+++ Assignment 2/root_finder.py
@@ -0,0 +1,56 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.optimize import root_scalar
+from functools import partial
+
+def f(lmbda, v0):
+    k = np.sqrt(lmbda)
+    kappa = np.sqrt(v0 - lmbda)
+
+    kappa_sin = kappa * np.sin(k/3)
+    k_cos = k * np.cos(k/3)
+
+    return np.exp(kappa/3) * (kappa_sin + k_cos)**2 - np.exp(-kappa/3) * (kappa_sin - k_cos)**2
+
+def plot_f(v0, N=1000, eig_vals=[]):
+    lmbdas = np.linspace(0, v0, N)
+
+    fig, ax = plt.subplots()
+    plt.plot(lmbdas, f(lmbdas, v0), label=r"$f(\lambda)$")
+    y_lims = ax.get_ylim()
+    
+    # Plot eigenvalues from numerical scheme
+    if len(eig_vals):
+        plt.vlines(eig_vals, *y_lims, linestyles="--", color="black", label="Numerical\neigenvalues")
+
+        # Text
+        text_i = []
+        for i in range(len(eig_vals)):
+            if i < len(eig_vals) - 1 and abs(eig_vals[i] - eig_vals[i+1]) < 10:
+                text_i.append(str(i+1))
+            else:
+                text_i.append(str(i+1))
+                plt.text(eig_vals[i]+10, y_lims[1] * (1-3e-2), f"n={','.join(text_i)}")
+                text_i = []
+
+
+    plt.xlabel(r"$\lambda$")
+    plt.ylabel(r"$f(\lambda)$")
+    plt.legend(loc="center right")
+    # plt.title("")
+    plt.show()
+
+def find_root(func, init_guess):
+    return root_scalar(func, x0=init_guess).root
+
+def find_roots(v0, eig_vals):
+    roots = []
+    for i, eig_val in enumerate(eig_vals):
+        init_guess = eig_val + 3 * (-1)**((i+1)%2)
+        roots.append(find_root(partial(f, v0=v0), init_guess=init_guess))
+
+    for i in range(len(roots)):
+        print(f'Lmbda={eig_vals[i]} -- Root={roots[i]} -- Diff={abs(eig_vals[i]-roots[i])}')
+
+if __name__ == '__main__':
+    plot_f(1e3)
--- Assignment 2/schrodinger.py
+++ Assignment 2/schrodinger.py
@@ -1,5 +1,8 @@
 # Created by Sami Laubo 26.02.2024
 
+%load_ext autoreload
+%autoreload 2
+
 import numpy as np
 import matplotlib.pyplot as plt
 from numba import njit
@@ -7,9 +10,12 @@ import glob
 import time
 import matplotlib.animation as animation
 from scipy.constants import hbar
+import os
+
+import root_finder
 
 class Schrodinger:
-    def __init__(self, L, Nx, Nt=1, T=1, m=1, pot_type="well", v0=0) -> None:
+    def __init__(self, L=1, Nx=1000, Nt=1, T=1, m=1, pot_type="well", v0=0) -> None:
         # Values
         self.L = L
         self.T = T
@@ -164,7 +170,7 @@ class Schrodinger:
 
 
 
-    def evolve(self, plot=True):
+    def evolve(self, plot=True, animate=False):
         # Load eigenvalues and vectors
         # eig_vals, eig_vecs = self.load_eigs()
         alpha = np.zeros(len(self.eig_vals))
@@ -174,26 +180,68 @@ class Schrodinger:
             alpha[n] = self.alpha_n(self.eig_vecs[:, n], self.Psi_0)
 
         # Evolve
-        # @njit
-        def f(Nt, Nx, t_, eig_vals, eig_vecs):
-            Psi = np.zeros((Nt, Nx), dtype=np.complex128)
-            for idx, t in enumerate(t_):
-                Psi[idx] = np.sum(alpha * np.exp(-1j*eig_vals*t) * eig_vecs, axis=1)
-
-                # Normalize
-                Psi[idx] /= np.sqrt(np.trapz(Psi[idx]*np.conj(Psi[idx])))
-            
-            return Psi
-        
-        Psi = f(self.Nt, self.Nx, self.t_, self.eig_vals, self.eig_vecs)
-
-        # Plot Psi
         if plot:
+            # @njit
+            def f(Nt, Nx, t_, eig_vals, eig_vecs):
+                Psi = np.zeros((Nt, Nx), dtype=np.complex128)
+                for idx, t in enumerate(t_):
+                    Psi[idx] = np.sum(alpha * np.exp(-1j*eig_vals*t) * eig_vecs, axis=1)
+
+                    # Normalize
+                    Psi[idx] /= np.sqrt(np.trapz(Psi[idx]*np.conj(Psi[idx])))
+                
+                return Psi
+            
+            Psi = f(self.Nt, self.Nx, self.t_, self.eig_vals, self.eig_vecs)
+
+            # Plot Psi
             plt.figure()
             for i in range(5):
-                plt.plot(self.x_, np.real(Psi[Psi.shape[0]//5*i]), label=f"t={self.t_[Psi.shape[0]//5*i]:.2e}")
+                Prob_dens = np.conj(Psi[Psi.shape[0]//5*i]) * Psi[Psi.shape[0]//5*i]
+                plt.plot(self.x_, Prob_dens, label=f"t={self.t_[Psi.shape[0]//5*i]:.2e}")
+                # plt.plot(self.x_, Psi[Psi.shape[0]//5*i], label=f"t={self.t_[Psi.shape[0]//5*i]:.2e}")
             plt.legend()
             plt.show()
+
+        # Animation
+        if animate:
+            fig, ax = plt.subplots()
+            plt.ylim([-0.001, 0.01])
+
+            # Psi_0
+            line, = ax.plot(self.x_, self.Psi_0, label=r"t'=0.00s")
+            self.plot_insert_potential(fig, ax, pad=0.001)
+            legend = plt.legend(loc="upper center")
+            plt.title("Probability density\nInitial wavefunction = " + r"$(\Psi_1 + \Psi_2)/\sqrt{2}$")
+            plt.xlabel("x'")
+            plt.ylabel(r"$|\Psi(x', t')|^2$")
+
+            def anim_func(i):
+                # Calculate and normalize
+                Psi = np.sum(alpha * np.exp(-1j*self.eig_vals*self.t_[i]) * self.eig_vecs, axis=1)
+                Psi /= np.sqrt(np.trapz(Psi*np.conj(Psi)))
+
+                line.set_ydata(np.conj(Psi)*Psi)
+                new_label = r"t'=" + f"{self.t_[i]:.2f}s"
+                legend.get_texts()[0].set_text(new_label)
+                return line,
+            
+            anim = animation.FuncAnimation(
+                fig,
+                anim_func,
+                len(self.t_),
+                interval = 1,
+                repeat=False,
+                blit=True
+            )
+
+            # plt.legend()
+
+            path = "output/t33_prob_dens/test1.gif"
+            if os.path.exists(path):
+                os.remove(path)
+            anim.save(path, fps=60)
+            plt.close()
 
     def plot_eig_values(self, Schrodinger_2=None, n_eig_vecs=4, n_eig_vals=10, plot_vals_n=False):
             
@@ -214,8 +262,8 @@ class Schrodinger:
                 plt.plot(Schrodinger_2.x_[::skip], Schrodinger_2.eig_vecs[:, i][::skip], 'x', color=cmap(i))
             
         plt.title("Eigenfunctions")
-        plt.ylabel("$\Psi(x)$")
-        plt.xlabel("x")
+        plt.ylabel("$\Psi(x')$")
+        plt.xlabel("x'")
         plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
         plt.tight_layout()
         plt.show()
@@ -262,13 +310,13 @@ class Schrodinger:
         self.plot_insert_potential(fig, ax)
             
         plt.title("Initial wavefunction")
-        plt.ylabel("$\Psi(x)$")
-        plt.xlabel("x")
+        plt.ylabel("$\Psi(x')$")
+        plt.xlabel("x'")
         plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
         plt.tight_layout()
         plt.show()
 
-    def plot_insert_potential(self, fig, ax, true_size=False):
+    def plot_insert_potential(self, fig, ax, true_size=False, pad=0.0):
         plt.figure(fig)
 
         if true_size:
@@ -278,9 +326,35 @@ class Schrodinger:
             plt.plot(self.x_, self.pot, color="black")
         else:
             y_lims = ax.get_ylim()
-            plt.vlines(0, *y_lims, linestyles='--', color='black', label=r"V$^*$")
-            plt.vlines(1, *y_lims, linestyles='--', color='black')
-            plt.plot(self.x_, (self.pot/max(np.max(self.pot), 1)*0.5*y_lims[1]) + y_lims[0], '--', color="black")
+            plt.vlines(0, y_lims[0] + pad, y_lims[1] - pad, linestyles='--', color='black', label=r"V$^*$")
+            plt.vlines(1, y_lims[0] + pad, y_lims[1] - pad, linestyles='--', color='black')
+            plt.plot(self.x_, (self.pot/max(np.max(self.pot), 1)*0.5*y_lims[1]) + y_lims[0] + pad, '--', color="black")
+
+    # Task 3.6
+    def eigvals_under_barrier(self, v0_low, v0_high, N):
+        v0 = np.linspace(v0_low, v0_high, N)
+        lmbda_under = np.zeros_like(v0)
+
+        # Find #lambdas under barrier
+        for i, v00 in enumerate(v0):
+            # S = Schrodinger(pot_type="barrier", v0=v00)
+            # S.eigen()
+            self.v0 = v00
+            self.discretize_pot()
+            self.eigen()
+            lmbda_under[i] = np.where(self.eig_vals > v00)[0][0]
+
+        # Plot
+        plt.figure()
+        plt.plot(v0, lmbda_under)
+        plt.xlabel(r"$\nu_0 = \frac{2mL^2}{\hbar^2}\cdot V_0$")
+        plt.ylabel("Count")
+        plt.title("Number of eigenvalues less than barrier height")
+        plt.show()
+
+        # Return value where #lmbda shifts to 1
+        last_zero = np.where(lmbda_under>0)[0][0]-1
+        return v0[last_zero], v0[last_zero+1]
 
 
 
@@ -323,23 +397,43 @@ def Task_3():
 
     ## Task 3.2
     # With barrier
-    v0 = 1e3
-    S = Schrodinger(L=1, Nx=1000, pot_type="barrier", v0=v0, Nt=500)
-    S.eigen()
-    # S.plot_eig_values(n_eig_vecs=4)
+    # v0 = 1e3
+    # S = Schrodinger(L=1, Nx=1000, pot_type="barrier", v0=v0, Nt=100)
+    # S.eigen()
+    # # S.plot_eig_values(n_eig_vecs=4)
 
-    ## Task 3.3
-    t1 = time.time()
-    S.init_cond(name="eigenfuncs", eigenfunc_idxs=[0,2])
-    S.plot_Psi_0()
-    # Update end time
-    S.T = np.pi / (S.eig_vals[1] - S.eig_vals[0]) * S.t0
-    # Discretize t again
-    S.discretize_x_t()
-    # Evolve
-    S.evolve()
-    t2 = time.time()
-    print(f'Time: {t2 - t1}')
+    # # ## Task 3.3
+    # t1 = time.time()
+    # S.init_cond(name="eigenfuncs", eigenfunc_idxs=[1,2])
+    # S.plot_Psi_0()
+    # # Update end time
+    # S.T = np.pi / (S.eig_vals[2] - S.eig_vals[1]) * S.t0
+    # # Discretize t again
+    # S.discretize_x_t()
+    # # Evolve
+    # S.evolve(plot=False, animate=True)
+    # t2 = time.time()
+    # print(f'Time: {t2 - t1}')
+
+    ## Task 3.4
+    # Root finding
+    # Plot function
+    # root_finder.plot_f(v0, eig_vals=S.eig_vals[:np.where(S.eig_vals>v0)[0][0]])
+
+    ## Task 3.5
+    # Find root values
+    # root_finder.find_roots(v0, eig_vals=S.eig_vals[:np.where(S.eig_vals>v0)[0][0]])
+
+    ## Task 3.6
+    # S = Schrodinger(pot_type="barrier")
+    # # S.eigvals_under_barrier(v0_low=0, v0_high=1e5, N=10)
+
+    # # Find value where #lambda 0->1
+    # v0_low, v0_high = 22, 23
+    # for _ in range(10):
+    #     print(f'{v0_low, v0_high = }')
+    #     v0_low, v0_high = S.eigvals_under_barrier(v0_low=v0_low, v0_high=v0_high, N=10)
+
     
 
 
@@ -354,3 +448,12 @@ if __name__ == '__main__':
 # TODO:
     # Fix same size for Nx in loaded eigvals and Psi_0: is correct for 1000
     # Move mains to new py file
+
+
+# Questions:
+    # Task 3.3: Initial cond psi n=1,3?
+    # Task 3.3: No tunneling
+
+
+# Sammenligne res:
+    # Task 3.5 - Root values vs eigenvalues
