8f8fc4b0cc62532101f9137c8ae2b590e33298f5
Sami Laubo <slaubo99@gmail.com>
2024-04-29 20:41:14 +0200

    37a

--- Exam/main.py
+++ Exam/main.py
@@ -26,7 +26,8 @@ TASK_22_d = False
 TASK_22_e = False
 TASK_22_f = False
 
-TASK_35_c = True
+TASK_35_c = False
+TASK_37_a = True
 
 
 # Timer class
@@ -42,24 +43,18 @@ class Timer:
         print(f'Task {self.task} time: {self.t2 - self.t1:.4e}s')
 
 # Plotting setup
-# Plot params
 plt.style.use('seaborn-v0_8-whitegrid')
 fontsize = 12
 plt.rcParams.update({
     "font.size": fontsize,
-    # "axes.titlesize": fontsize,
-    # "axes.labelsize": fontsize,
-    # "ytick.labelsize": fontsize,
-    # "xtick.labelsize": fontsize,
-    # "legend.fontsize": fontsize,
     "legend.frameon": True,
     "mathtext.fontset": "stix",
     "font.family": "STIXGeneral"
 })
 from matplotlib import rc
 rc('text', usetex=True)
-# from IPython.display import set_matplotlib_formats
-# set_matplotlib_formats("svg")
+
+# With Jupyter / IPykernel
 %matplotlib inline
 import matplotlib_inline
 matplotlib_inline.backend_inline.set_matplotlib_formats('svg')
@@ -191,19 +186,20 @@ def Task_2():
 def Task_3():
     timer = Timer()
 
-    # Parameters
-    a = 0.0 # [m]
-    b = 20.0 # [m]
-    x0 = (b-a)/2
-    Nx = 300
-    Nt = 400
-    T = 2.0
         
     if TASK_35_c: 
         timer.start("3.5c")
 
+        # Parameters
+        a = 0.0 # [m]
+        b = 20.0 # [m]
+        x0 = (b-a)/2
+        Nx = 300
+        Nt = 400
+        T = 2.0
+
         # Analytical with own values
-        neuron_analytical = Neuron(a, b, x0, Nx, T, Nt)
+        neuron_analytical = Neuron(a, b, x0, Nx, T, Nt=4)
         V_analytical = neuron_analytical.cable_analytical()
         
         # Schemes
@@ -215,9 +211,9 @@ def Task_3():
         fig, axs = plt.subplots(1, 4, figsize=(20,5))
         axs = axs.ravel()
         neuron_analytical.plot_evolution(V_analytical, ax=axs[0])
-        neuron.plot_evolution(V_explicit, ax=axs[1], plot_times=neuron_analytical.t[1:])
-        neuron.plot_evolution(V_implicit, ax=axs[2], plot_times=neuron_analytical.t[1:])
-        neuron.plot_evolution(V_crank, ax=axs[3], plot_times=neuron_analytical.t[1:])
+        neuron.plot_evolution(V_explicit, ax=axs[1])#, plot_times=neuron_analytical.t[1:])
+        neuron.plot_evolution(V_implicit, ax=axs[2])#, plot_times=neuron_analytical.t[1:])
+        neuron.plot_evolution(V_crank, ax=axs[3])#, plot_times=neuron_analytical.t[1:])
 
         axs[0].set_title("Analytical")
         axs[1].set_title("Implicit Euler")
@@ -233,6 +229,38 @@ def Task_3():
         plt.figure(fig)
         fig.savefig("output/task_2/t35c_schemes_compare.pdf")
         plt.show()
+
+        timer.end()
+
+
+    if TASK_37_a: 
+        timer.start("3.7a")
+
+        # Parameters
+        a = 0.0 # [m]
+        b = 1000.0e-3 # [m]
+        x0 = (b-a)/2
+        Nx = 300
+        Nt = 10
+        T = 1.0 # [s]
+
+        # Physiological parameters
+        lmbda = 0.18e-3 # [m]
+        tau = 2.0e-3 # [s]
+        g_K = 5.0 # [Ohm^-1 m^-2]
+        V_thr = -40e-3 # [V]
+        gamma = 0.5e-3 # [V^-1]
+        VN_Na = 56.0e-3 # [V]
+        VN_K = -76.0e-3 # [V]
+        V_appl = -50.0e-3 # [V]
+        V_mem = -70.0e-3 # [V]
+
+        neuron = Neuron(a, b, x0, Nx, T, Nt,
+                        lmbda=lmbda, tau=tau, g_K=g_K, V_thr=V_thr, gamma=gamma, 
+                        VN_Na=VN_Na, VN_K=VN_K, V_appl=V_appl, V_mem=V_mem)
+        
+        V_crank = neuron.evolve_scheme(scheme="crank-nicolson", extra_eq=True)
+        neuron.plot_evolution(V_crank, text_pad=0.2e-3)
 
         timer.end()
 
--- Exam/neuron_electrical.py
+++ Exam/neuron_electrical.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 
 class Neuron:
-    def __init__(self, a, b, x0, Nx, T, Nt, lmbda=1, tau=1):
+    def __init__(self, a, b, x0, Nx, T, Nt, **kwargs):
         # Tau and Lambda is set to 1 always
         self.a = a
         self.b = b
@@ -12,15 +12,24 @@ class Neuron:
         self.T = T
         self.Nx = Nx
         self.Nt = Nt
-        self.lmbda = lmbda
-        self.tau = tau
         self.x, self.dx = np.linspace(a, b, Nx + 1, retstep=True)
         self.t, self.dt = np.linspace(0, T, Nt + 1, retstep=True)
         self.x_x0_2 = (self.x - x0)**2
 
-        print(f'alpha = {lmbda**2*self.dt / (self.dx**2 * tau)}')
+        # Physilogical params
+        self.lmbda = kwargs.pop("lmbda", 1)
+        self.tau = kwargs.pop("lmbda", 1)
+        self.g_K = kwargs.pop("g_k", 1)
+        self.V_thr = kwargs.pop("V_thr", 1)
+        self.gamma = kwargs.pop("gamma", 1)
+        self.VN_Na = kwargs.pop("VN_Na", 1)
+        self.VN_K = kwargs.pop("VN_K", 1)
+        self.V_appl = kwargs.pop("V_appl", 1)
+        self.V_mem = kwargs.pop("V_mem", 1)
 
-    def create_matrices_AB(self, scheme="Crank-Nicolson"):
+        print(f'alpha = {self.lmbda**2*self.dt / (self.dx**2 * self.tau)}')
+
+    def create_matrices_AB(self, scheme="Crank-Nicolson", extra_eq=False):
         # Coefficients
         alpha = self.lmbda**2 * self.dt / (self.dx**2 * self.tau)
         beta = self.dt / self.tau
@@ -48,28 +57,46 @@ class Neuron:
             np.fill_diagonal(A[1:], -alpha/2)
             np.fill_diagonal(A[:, 1:], -alpha/2)
 
-            np.fill_diagonal(B, 1 - alpha - beta)
+            if extra_eq:
+                np.fill_diagonal(B, 1 - alpha)
+            else:
+                np.fill_diagonal(B, 1 - alpha - beta)
             np.fill_diagonal(B[1:], alpha/2)
             np.fill_diagonal(B[:, 1:], alpha/2)
 
         return A, B
 
-    def evolve_scheme(self, scheme="Crank-Nicolson"):
+    def evolve_scheme(self, scheme="Crank-Nicolson", extra_eq=False):
         
         # Matrix to store V for each timestep
         V = np.zeros((self.Nt+1, self.Nx+1))
 
         # Initial potential
-        V[0] = np.exp(-self.x_x0_2)
-        V[0] = V[0] / np.sum(V[0])
+        if extra_eq:
+            V[0] = (self.V_appl - self.V_mem) * np.exp(-self.x_x0_2/(2*self.lmbda**2)) + self.V_mem
+        else:
+            V[0] = np.exp(-self.x_x0_2)
+            V[0] = V[0] / np.sum(V[0])
 
         # Create matrices
-        A, B = self.create_matrices_AB(scheme=scheme)
+        A, B = self.create_matrices_AB(scheme=scheme, extra_eq=extra_eq)
+        B_diag_idx = np.diag_indices_from(B)
+        B2 = np.zeros_like(B)
 
         # Solve over time
-        for i in range(1, self.Nt+1):
-            b = np.dot(B, V[i-1])
-            V[i] = np.linalg.solve(A, b)
+        if extra_eq:
+            beta = self.dt / self.tau
+            for i in range(1, self.Nt+1):
+                # B2 = beta / self.g_k *
+                B2 = B[:]
+                B2[B_diag_idx] -= beta * (100/(1+np.exp(self.gamma*(self.V_thr-V[i-1]))) + 1/5) / self.g_K \
+                                       * (V[i-1] - self.VN_Na) + beta * ((V[i-1] - self.VN_K))
+                b = np.dot(B2, V[i-1])
+                V[i] = np.linalg.solve(A, b)
+        else:
+            for i in range(1, self.Nt+1):
+                b = np.dot(B, V[i-1])
+                V[i] = np.linalg.solve(A, b)
 
         return V[1:]
 
@@ -88,11 +115,11 @@ class Neuron:
 
         return V
     
-    def plot_evolution(self, V, path="", ax=None, plot_times=None):
+    def plot_evolution(self, V, path="", ax=None, plot_times=None, text_pad=5):
         if ax is None:
             fig, ax = plt.subplots(figsize=(8,5))
 
-        if plot_times is not None:
+        if plot_times is None:
             idxs = np.linspace(0, len(V)-1, 4, dtype=int)
         else:
             idxs = []
@@ -107,8 +134,8 @@ class Neuron:
             x_max = self.x[np.argmax(V[i])]
             y_max = np.max(V[i])
 
-            ax.plot([x_max, x_max+5], [y_max]*2, "--", color="k", linewidth=0.7)
-            ax.text(x_max + 5,
+            ax.plot([x_max, x_max+text_pad], [y_max]*2, "--", color="k", linewidth=0.7)
+            ax.text(x_max + text_pad,
                     y_max, 
                     f"t = {self.t[i+1]:.2f}s", 
                     horizontalalignment="left",
